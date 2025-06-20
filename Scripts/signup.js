99c61d665e6157bd32cf284c900bd1a7b3491609
nicolas m <confrumack.atop806@passinbox.com>
2025-06-20 13:48:38 +0200

    signup now dynamic

---
+++ Scripts/signup.js
@@ -0,0 +1,5 @@
+import { signupInner } from "./view/signupinner.js";
+import { addHeaderEventListeners } from "./controler/homepagecontroler.js";
+
+signupInner();
+addHeaderEventListeners();
---
+++ Scripts/view/signupinner.js
@@ -0,0 +1,45 @@
+export function signupInner() {
+  const templatesignup = `<header>
+    <nav>
+            <div class="logo">
+                <img src="Assets/logo ToolHub.png" alt="logo">
+                <h1>ToolHub</h1>
+        </div>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="tools.html">Tools</a></li>
+            <li><a href="ressource.html">Resources</a></li>
+        </ul>
+        <div class="login">
+            <button href="login.html">Login</button>
+            <button href="signup.html">Signup</button>
+        </div>
+    </nav> 
+</header>
+
+ <main> 
+    <section>
+
+    <form class="signup-container">
+        <h2>Create an Account</h2>
+        <label for="username"><span>Username:</span></label>
+        <input type="text" id="username" name="username" placeholder="your username" required>
+        <label for="email"><span>E-mail:</span></label>
+        <input type="email" id="email" name="email" placeholder="your e-mail" required>
+        <label for="password"><span>Password:</span></label>
+        <input type="password" id="password" name="password" placeholder="your password" required>
+        <label for="confirm-password"><span>Confirm Password:</span></label>
+        <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm your password" required>
+        <button type="submit">Signup</button>
+        <div class="login-link">
+            Already got an account? <a href="login.html">login</a>
+        </div>
+    </form>
+    </section>
+     <footer class="site-footer">
+        <div class="footer-content">
+            <p>&copy; 2025 ToolHub. All rights reserved.</p>
+        </div>
+     </main>`;
+  document.body.innerHTML = templatesignup;
+}
